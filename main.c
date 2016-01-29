de2bdcc2cdcbd4327eddba7a46bd136a379afd3f
Naoto Iida <iida@h83.153.228.10.30824.vlan.kuins.net>
2016-01-30 04:21:57 +0900

    Implementation of command line arguments.

--- main.c
+++ main.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]){
-	staInfo sta[numSta];
-	apInfo ap;
-	traceinfo trace;
+	if(argc>1){
+		char *help = argv[1];
 
+		if(strcmp(help, "h")==0){
+			printf(
+			"2: 'h' shows help.\n"
+			"3: STA amount.\n"
+			"4: Simulation time (sec).\n"
+			);
+		}
+		exit(1);   //Showing help.
+	}
 
+	if(argc!=2){
+		printf("The number of options is wrong\n");
+		exit(2);
+	}
 
 	return 0;
 }
